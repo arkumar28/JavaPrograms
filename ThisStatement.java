47f5a4158141c8b2fb0abe097546fdf57baae861
arkumar28 <arkumar28@yahoo.com>
2024-09-25 14:14:04 +0530

    Latest Code Upto 09_25

---
+++ ThisStatement.java
@@ -0,0 +1,27 @@
+package basicsprograms;
+
+public class ThisStatement {
+
+	ThisStatement(int a, int b){
+		System.out.println("Addition of a and b "+ (a+b));
+	}
+	
+	ThisStatement(String a){
+		this();
+		System.out.println(a);
+	}	
+	
+	ThisStatement(){
+		this(10,11);
+		System.out.println("This Calling Statement");
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ThisStatement("Arun");
+		
+		
+	}
+
+}
